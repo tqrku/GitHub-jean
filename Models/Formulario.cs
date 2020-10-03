8b33f36622e06f3ddab4b8528d4e524c92eef8b6
JeanPieer <jean_huaman@usmp.pe>
2020-10-03 14:33:23 -0500

    Creacion del modelo formulario

---
+++ Models/Formulario.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace formulario.Models
+{
+    public class formulario
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+
+        public String email { get; set; }
+
+        public int telefono { get; set; }
+        public String subject { get; set; }
+        public String mensaje { get; set; }
+    }
+}
