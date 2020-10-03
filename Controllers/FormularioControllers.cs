ff691e24919ade59b9ab3f76468540c23af7d817
JeanPieer <jean_huaman@usmp.pe>
2020-10-03 14:22:33 -0500

    creacion del controlador de formularios

---
+++ Controllers/FormularioControllers.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using formulario.Models;
+
+namespace formulario.Controllers
+{
+    public class FormularioController : Controller
+    {
+        private readonly ILogger<FormularioController> _logger;
+
+        public FormularioController(ILogger<FormularioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
