1f3af31b854658d92fb915b78fb5de3a4cb83e5e
JeanPieer <jean_huaman@usmp.pe>
2020-10-14 11:14:42 -0500

    Creacion del controllador contacto

---
+++ Formulario/Controllers/ContactoController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using formulario.Models;
+
+namespace formulario.Controllers
+{
+    public class ContactoController : Controller
+    {
+         private readonly ILogger<ContactoController> _logger;
+
+        public ContactoController(ILogger<ContactoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(Contacto objContacto){
+            if (ModelState.IsValid)
+            {
+                objContacto.Response = "Gracias estamos en contacto";
+            }
+            return View("index", objContacto);
+        }
+
+       
+    }
+}
