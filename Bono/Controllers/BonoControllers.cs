ff922c8a7fdb8410900c9fab84a8499d0970bf7d
JeanPieer <jean_huaman@usmp.pe>
2020-10-18 16:10:28 -0500

    Controlador de la vista de los registros de bono

---
+++ Bono/Controllers/BonoControllers.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bono.Models;
+using Bono.Data;
+
+namespace Bono.Controllers
+{
+    public class BonoController : Controller
+    {
+
+       private readonly ILogger<BonoController> _logger;
+       private readonly DatabaseContext _context;
+
+
+        public BonoController(ILogger<BonoController> logger,
+            DatabaseContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(Bono objBono){
+            if (ModelState.IsValid)
+            {
+                _context.Add(objBono);
+                _context.SaveChanges();
+                objBono.Response = "Se  ha registrado para recibir el bono";
+            }
+            return View("index", objBono);
+        }
+
+    }
+}
