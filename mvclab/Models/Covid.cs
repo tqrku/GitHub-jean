f76ff2a6f85d1f8669d386d1a89e7256d15db705
JeanPieer <jean_huaman@usmp.pe>
2020-10-19 11:02:06 -0500

    cambios de la base de datos

---
+++ mvclab/Models/Covid.cs
@@ -0,0 +1,49 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvcLab.Models
+{
+    [Table("t_bono")]
+    public class Covid
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name="Name")]
+        [Column("name")]
+        public String Nombre{ get; set; }
+
+        [Required]
+        [Display(Name="LastName")]
+        [Column("lastname")]
+        public String Apellido{ get; set; }
+
+        [Required]
+        [Display(Name="District")]
+        [Column("district")]
+        public String Distrito{ get; set; }
+
+        [Required]
+        [Display(Name="Bank")]
+        [Column("bank")]
+        public String Banco{ get; set; }
+
+        [Required]
+        [Display(Name="Age")]
+        [Column("age")]
+        public int Edad{ get; set; }
+
+
+        [Display(Name="Author")]
+        [Column("author")]
+        public String Autor{ get; set; }
+
+        [NotMapped]
+        [Display(Name="Output")]
+        public String Respuesta { get; set; }
+    }
+}
