9020554658d3bddc731f70c9ac2affa20be78bba
JeanPieer <jean_huaman@usmp.pe>
2020-10-19 11:01:24 -0500

    instalacion de los drivers

--- mvclab/Startup.cs
+++ mvclab/Startup.cs
@@ -9,6 +9,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using mvcLab.Data;
+
 namespace mvclab
 {
     public class Startup
@@ -23,6 +27,8 @@ namespace mvclab
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("postgress-db")));
             services.AddControllersWithViews();
         }
 
