e75514315aebdf132e60a6beb7ba613f3f94fd33
JeanPieer <jean_huaman@usmp.pe>
2020-12-10 21:54:34 -0500

    Build aplication

---
+++ ProducctosApp/Models/Producto.cs
@@ -0,0 +1,16 @@
+namespace ProducctosApp.Models
+{
+    public class Producto
+    {
+        public int Id {get; set;}
+        public string Nombre {get; set;}
+        public string Url {get; set;}
+        public string Descripcion {get; set;}
+        public int Precio {get; set;}
+        public string Celular {get; set;}
+        public string Lugar {get; set;}
+        public string NombreUsuario {get; set;}
+        
+
+    }
+}
