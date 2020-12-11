b1ad79705d117018155f1bfccc6a1b940164be8a
JeanPieer <jean_huaman@usmp.pe>
2020-12-10 22:30:45 -0500

    build Producto

--- ProducctosApp/Models/Producto.cs
+++ ProducctosApp/Models/Producto.cs
@@ -1,14 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 namespace ProducctosApp.Models
 {
     public class Producto
     {
         public int Id {get; set;}
+        [Required]  
         public string Nombre {get; set;}
+        [Required] 
         public string Url {get; set;}
+        [Required] 
         public string Descripcion {get; set;}
+        [Required] 
         public int Precio {get; set;}
+        [Required] 
         public string Celular {get; set;}
         public string Lugar {get; set;}
+        [Required] 
         public string NombreUsuario {get; set;}
         
 
